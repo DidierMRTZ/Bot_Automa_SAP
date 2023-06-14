0e4212ec92079297109548b621e0669a1638ab0d
PRACTICANTE INGENIERIA INDUSTRIAL 2 <prac.ingindustrial2@prebel.com.co>
2023-06-14 11:27:00 -0500

    Actualizar libreria funviones

---
+++ Automatizaciones Prebel/Bot_Disnal_Pendientes/Librerias_SAP/Funtions.py
@@ -0,0 +1,54 @@
+import re
+
+
+"""----------------------------------------------CLEAN COLUMN AND DATA-----------------------------------------------------------"""
+def Clean_Columns(List):
+    Set_Columns=[i.strip() for i in List.columns]   #Alert in list
+    clean_column=[i if "Unnamed" in i else None for i in List.columns]
+    clean_column = list(filter(lambda x: x is not None, clean_column)) 
+    List=List.set_axis(Set_Columns, axis=1).drop(clean_column,axis=1)  #Drop Unammed: 0
+    return(List)
+
+"""------------------------------------------FUNCION PARA CAMBIAR A NUMEROS-------------------------------------------------------"""
+
+def Clean_num(x):
+    x = float(str(x).strip().replace(',',''))
+    return(x)
+
+"""------------------------------------------FUNCION PARA COMPLETAR los 10 con 00 al inicio---------------------------------------"""
+def Complete_00(lista):
+    Nueva_Lista_Ordenes=[]
+    for i in lista:
+        while len(str(i))<10:
+            i="0"+i
+            # print(len(str(i)),i)
+        Nueva_Lista_Ordenes.append(str(i))
+    return(Nueva_Lista_Ordenes)
+
+def default_column(default_columns,dataframe):     #Parametros (default_columns: Columnas predeterminadas,dataframe:)
+    diccionay_default_column={}
+    if len(default_columns)==len(dataframe.columns):
+        for i in range(0,len(default_columns)):
+            diccionay_default_column[dataframe.columns[i]]=default_columns[i]   
+        dataframe=dataframe.rename(columns=diccionay_default_column)   #Remplazo las columnas con las de default
+    else:
+        None   #asumo columnas originales como estandar
+    return(dataframe)
+
+
+# Ecluir Agenda de pedidos exito
+
+def Datos_Agenda(data_pedido,Agenda):   #(data_pedido: columna dataframe a transformar, Agenda: Datos a encontrar)
+    Exluidos_Entrega=set()
+    for i in data_pedido:
+        if (str(i)[:4] in Agenda) or (str(i)[:3] in Agenda):
+            Exluidos_Entrega.add(i)
+    return(Exluidos_Entrega)
+
+
+def Search_agenda(data_Pedidos,agenda):
+    conjunto_agenda=set()
+    for i in data_Pedidos:
+        if re.findall("(\d*)-",str(i))!=[] and (re.findall("(\d*)-",str(i))[0] in agenda):
+            conjunto_agenda.add(i)
+    return(conjunto_agenda)
