e86c4a2fb6595d5ad6fc6543b359bf9a8a89fd64
DidierMRTZ <dsmartinezbu@unal.edu.co>
2023-06-15 07:51:19 -0500

    Copio funcion buscar ZSD110

--- Automatizaciones Prebel/Bot_Disnal_Pendientes/Librerias_SAP/SAP_GUI.py
+++ Automatizaciones Prebel/Bot_Disnal_Pendientes/Librerias_SAP/SAP_GUI.py
@@ -231,3 +231,21 @@ def Close_session(session):
     session.findById("wnd[0]").close()
     session.findById("wnd[1]/usr/btnSPOP-OPTION1").press()
 
+
+
+
+"""------------------------------------------ START Search ZD110----------------------------------------------------"""
+def Search_ZSD110(Transsaccion,variant,provision,session):  #Optiene la tabla al buscar la transaccion
+    session.StartTransaction(Transsaccion)
+    session.findById("wnd[0]/tbar[1]/btn[17]").press()
+    Varians_ZSD110=session.findById("wnd[1]/usr/cntlALV_CONTAINER_1/shellcont/shell")
+    Rows_Varians_ZSD110=Varians_ZSD110.RowCount
+    # List Variants
+    List_Varian_ZSD110=[Varians_ZSD110.GetCellValue(i,"VARIANT") for i in range(0,Rows_Varians_ZSD110)] 
+    indice=[indice for indice, dato in enumerate(List_Varian_ZSD110) if dato == variant]
+    Varians_ZSD110.selectedRows = indice[0]
+    session.findById("wnd[1]/tbar[0]/btn[2]").press()
+    session.findById("wnd[0]/usr/ctxtPA_LAYOU").text = provision
+    session.findById("wnd[0]/tbar[1]/btn[8]").press()
+    tabla_zsd110=session.findById("wnd[0]/usr/cntlGRID1/shellcont/shell")     #Select table cont
+    return(tabla_zsd110)
